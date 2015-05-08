338429e1e0ac41698f5008d2950aaf3f04327e1b
Caio Homar <caioah@users.noreply.github.com>
2015-05-08 20:36:19 -0300

    Update wordlist.rb

--- wordlist.rb
+++ wordlist.rb
@@ -8,9 +8,9 @@ Numbers = 4
 Others = 8
 class Wordlist
     attr_accessor :alpha, :upcase, :num, :other, :custom, :alphabet
-    def initialize(opt = 1, cus = "")
+    def initialize(opt = 1, cust = nil)
         @alpha,@upcase,@num,@other = [AlphaLow,AlphaUp,Numbers,Others].map{|x| (opt & x) == x}
-        @custom = cus
+        @custom = (cust.nil? ? "" : cust)
         @alphabet = (@custom.chars |
                 ((@alpha ? AlphaL : "") + 
                 (@upcase ? AlphaU : "") +
@@ -20,9 +20,7 @@ class Wordlist
     end
     def fixed(n,&b)
         @alphabet.chars.repeated_permutation(n).map{|x| 
-            tmp = x.join
-            b.call(tmp) if not b.nil?
-            tmp}
+            ((not b.nil?) ? b.call(tmp) : x.join)}
     end
     def variable(s,e,&b)
         return nil if s <= 0
@@ -33,25 +31,29 @@ class Wordlist
     def masked(mask,&b)
         Wordlist.masked(mask,@custom,&b)
     end
-    def self.masked(mask,cus = "",&b)
+    def self.masked(mask,cust = "",&b)
         tmp = case mask[0]
             when "a" then AlphaL
             when "A" then AlphaU
             when "#" then Num
             when "@" then Other
-            when "*" then cus
+            when "*" then cust
             else raise "Invalid mask: #{mask[0]}"
         end
-        return masked(mask[1..-1],cus,&b) if tmp.empty? and mask.length > 1
+        if tmp.empty?
+            return masked(mask[1..-1],cust,&b) if mask.length > 1
+            return []
+        end
         tmp.chars.map{|x| 
             if mask.length > 1
-                masked(mask[1..-1],cus).map{|y| 
-                    tmp2 = x+y
-                    b.call(tmp2) if not b.nil?
-                    tmp2}
+                a = masked(mask[1..-1],cust)
+                (a.empty? ? 
+                    ((not b.nil?) ? b.call(x) : x) : 
+                    a.map{|y| 
+                        w = x+y
+                        ((not b.nil?) ? b.call(w) : w)})
             else 
-                b.call(x) if not b.nil?
-                x
+                ((not b.nil?) ? b.call(x) : x)
             end}.flatten   
     end
 end
